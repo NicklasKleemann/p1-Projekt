e5882e37d41b51c0250cee2492d204b923e764c4
NicklasKleemann <71381595+NicklasKleemann@users.noreply.github.com>
2022-11-23 15:40:24 +0100

    wasd

---
+++ P1.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void ConstructArray(char ** arr, FILE * file){
+    for( int i = 0 ; i < 128 ; ++i){
+        for(int j = 0; j < 20 && fgetc(file)!=' ' && fgetc(file)!=EOF; ++j){
+            arr[i][j] = fgetc(file);
+            printf("%c", arr[i][j]);
+        }
+    }
+}
+void printArr(char ** arr){
+    int i, j;
+    for(i = 0; i <128; ++i){
+        for (j= 0; j < 20; ++j){
+            if(arr[i][j]==' ')break;
+            printf("%c", arr[i][j]);
+        }
+        if(j>1)printf(" ");
+    }
+}
+
+int main(int argc, char **argv){
+    FILE *Original;
+    FILE *Kontrol;
+
+    FILE *SynListe; //Synonym liste
+
+    char original[128][20]; //Mængde af ord og max længde på ord
+    char kontrol[128][20];
+    
+    Original = fopen("t1.txt", "r");
+    Kontrol = fopen("t2.txt", "r");
+
+    ConstructArray(original, Original);
+    ConstructArray(kontrol, Kontrol);
+
+    //printArr(original);
+    //printArr(kontrol);
+
+
+    return 0;
+}
