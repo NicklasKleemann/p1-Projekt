6614e83b647dad1dba46ccd92692647272c534ad
NicklasKleemann <71381595+NicklasKleemann@users.noreply.github.com>
2022-11-23 13:09:53 +0100

    wasd

--- P1Projekt.c
+++ P1Projekt.c
@@ -2,8 +2,11 @@
 #include <sqlite-amalgamation-3400000\sqlite3.h> 
 
 int main(int argc, char **argv){
-    
+    sqlite3 *db;
+    char *errmsg = 0;
+
+    int database = sqlite3_open("Database/SynListe.db",&db);
     
 
-    return 0;
+    return 1;
 }
