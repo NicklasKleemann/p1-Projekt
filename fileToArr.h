919463b98a76a365a111c3816dd479fc0d2c622c
Lasse Drongesen <Lassemgp@gmail.com>
2022-12-08 12:34:41 +0100

    Headerfiles

---
+++ fileToArr.h
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+
+void dynamicFileTable(FILE *f, int *rows, int *cols, int isSym)
+{
+    int counter = 0;
+    char c;
+    c = fgetc(f);
+    while (c != EOF)
+    {
+        counter++;
+        if (counter > *cols)
+        {
+            *cols = counter;
+        }
+
+        if (isspace(c) && isSym == 0)
+        {
+            counter = 0;
+            *rows += 1;
+        }
+        if (c == '\n' && isSym == 1)
+        {
+            counter = 0;
+            *rows += 1;
+        }
+        c = fgetc(f);
+    }
+    // printf("\n%d rows \n%d cols\n", *rows, *cols); //USE FOR DEBUG
+    rewind(f);
+}
+
+void loadSynListe(FILE *file, int row3, int col3, char *arr)
+{ // LOAD SYNONYM LIST INTO ARRAY
+    int line = 0;
+    while (!feof(file) && !ferror(file))
+    {
+        if (fgets(arr + line * col3, col3 + 1, file) != NULL)
+        {
+            line++;
+        }
+    }
+}
+
+void ConstructArray(int rows, int cols, FILE *file, char *arr)
+{
+    char c;
+    int i, j = 0;
+    for (i = 0; i < rows && !feof(file); ++i)
+    {
+        do
+        {
+            c = fgetc(file);
+            if (isspace(c) || c == EOF) /* Optimering gjort her, uden c == EOF, -0\ i sidste ord og kan derfor ikke bruges som string */
+            {
+                if (c == '\n' && j < cols)
+                {
+                    *(arr + i * cols + j) = c;
+                    *(arr + i * cols + j + 1) = '\0';
+                }
+                else
+                {
+                    *(arr + i * cols + j) = '\0';
+                }
+            }
+            else
+            {
+                *(arr + i * cols + j) = c;
+            }
+
+            j++;
+        } while (j < cols && !isspace(c) && c != EOF);
+        j = 0;
+    }
+}
---
+++ plagiat.h
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+void printArr(int rows, int cols, char *arr, char *name) // PRINT INPUT FILE AND CONTROL FILE
+{
+    int i;
+    printf("\033[1;33m%s\033[0m:\n", name); // NAME OF EACH FILE
+    for (i = 0; i < rows; ++i)
+    {
+        printf(" ");
+        printf("%s", arr + i * cols);
+    }
+    printf("\n");
+}
+
+double compareFiles(int row1, int row2, int row3, int col1, int col2, int col3, char *arr1, char *arr2, char *Syn)
+{
+    printf("\nComparing files...\n");
+    int counter = 0, syncounter = 0;
+    int j = 0;
+
+    for (int i = 0; i < row1 && i < row2; ++i)
+    {
+        removePunct(arr1, col1, i);
+        removePunct(arr2, col2, i);
+
+        if (strcmp(arr1 + i * col1, arr2 + i * col2) == 0) // CHECK SIMILARITY
+        {
+            counter++;
+        }
+        else
+        {
+            if (SynCheck(arr1 + i * col1, arr2 + i * col2, row3, col3, Syn) == 1)
+            { // CHECK SYNONYM EVERY DIFFERENCE
+                counter++;
+                syncounter++;
+                printf("\033[1;32m%s\033[0;32m and\033[1;32m %s\033[0;32m are synonyms\033[0m\n", arr1 + i * col1, arr2 + i * col2);
+            }
+        }
+    }
+    printf("\nFound a total of\033[1;32m %d\033[0m synonym exchanges", syncounter);
+    return counter;
+}
+
+void removePunct(char *arr, int col, int i)
+{
+    // REMOVE PUNCTUATION FROM WORDS BEFORE COMPARING
+    if (ispunct(*(arr + i * col + strlen(arr + i * col) - 1)) || *(arr + i * col + strlen(arr + i * col) - 1) == '\n')
+    {
+        *(arr + i * col + strlen(arr + i * col) - 1) = '\0';
+    }
+}
+
+int SynCheck(char *ord, char *ord2, int row3, int col3, char *SynListe)
+{
+    int line_number = 0;
+    while (strstr(SynListe + line_number * col3, ord) == NULL && line_number < row3)
+    { // FIND "ord" FROM SYNLISTE
+        line_number++;
+    }
+
+    if (strstr(SynListe + line_number * col3, ord2) != NULL)
+    { // FIND "ord2" FROM SYNLISTE WHICH MATCHES THE SAME INDEX AS "ord"
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
